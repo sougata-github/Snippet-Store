0a7a9bbe7c447d7ac386f2fc340de752256ecc41
Sougata <sougatadas9874@gmail.com>
2024-07-18 21:50:42 +0530

    add footer and protect my-snippets route.

--- components/marketing/Footer.tsx
+++ components/marketing/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="w-full py-12 px-8 flex items-center justify-center">
+      <p className="text-sm text-black/50">@snippet-store | 2024</p>
+    </footer>
+  );
 };
 
 export default Footer;
